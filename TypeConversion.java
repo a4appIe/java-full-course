ec619d76be9ddf3578a0c0a7936454b250fa32a0
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 00:28:41 +0530

    type-conversion fixed

--- TypeConversion.java
+++ TypeConversion.java
@@ -1,8 +1,8 @@
 public class TypeConversion {
     public static void main(String[] args) {
-        byte b = 127;
+        // byte b = 127;
         int a = 258;
-        b = (byte) a;
+        // b = (byte) a;
         char c = 'a';
         a = (int) c;
         float fl = 12.6f;

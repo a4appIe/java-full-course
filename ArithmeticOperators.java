1df57a816312c46898179357a5e3756ed0150687
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 00:27:56 +0530

    arithmetic operators in java

---
+++ ArithmeticOperators.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperators {
+    public static void main(String[] args) {
+        int a = 8;
+        int b = 7;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a%b);
+        System.out.println(a++);
+        System.out.println(a--);
+        System.out.println(--a);
+        System.out.println(++a);
+    }
+}

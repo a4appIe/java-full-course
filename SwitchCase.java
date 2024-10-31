0527653d8620fff3cb27012a09ea54a4e2e42de0
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 01:21:29 +0530

    new switch-case in java

--- SwitchCase.java
+++ SwitchCase.java
@@ -1,32 +1,36 @@
 public class SwitchCase {
     public static void main(String[] args) {
-        int a = 9;
-        switch (a){
-            case 1:
-                System.out.println("Monday");
-                break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thursday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
-                break;
-            default: 
-                System.out.println("Enter a valid number");
+        String a = "monday";
+        // switch (a){
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sunday");
+        //         break;
+        //     default: 
+        //         System.out.println("Enter a valid number");
+        // }
+
+        switch (a) {
+            case "monday" -> System.out.println("monday");
+            case "tuesday" -> System.out.println("tuesday");
+            default -> System.out.println("sunday");
         }
-
-
     }
 }

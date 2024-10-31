2a14d9717b9fdd9c0bdc1019dba577fac596caae
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 00:47:59 +0530

    if-else in java

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+public class IfElse {
+    public static void main(String[] args) {
+        int speed = 50;
+        if(speed < 30){
+            System.out.println("increase speed");
+        }
+        if(speed == 50){
+            System.out.println("good speed");
+        }
+        if(speed > 60){
+            System.out.println("stop the car");
+        }
+    }
+}

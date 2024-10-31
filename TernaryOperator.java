8a1218b15150b9c094f2c90dfa24d94c3182f0c4
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 01:02:33 +0530

    ternary operator in java

---
+++ TernaryOperator.java
@@ -0,0 +1,8 @@
+public class TernaryOperator {
+    public static void main(String[] args) {
+        int a = 5;
+        int res = 0;
+        res = a > 5 ? 10 : 20;
+        System.out.println(res);
+    }
+}

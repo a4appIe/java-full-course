480543c526932dac191cb176aec8fc3720aadbd5
Anthony <nitinbhagatxyz2@gmail.com>
2024-10-31 13:31:36 +0530

    data-types in java

---
+++ DataTypes.java
@@ -0,0 +1,20 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        byte by = 12;
+        short sh = 123;
+        int in = 12324;
+        long lg = 123456754l;
+        char ch = 'A';
+        float fl = 5.5f;
+        double db = 5.6;
+        boolean bl = true;
+        System.out.println(by);
+        System.out.println(sh);
+        System.out.println(in);
+        System.out.println(lg);
+        System.out.println(ch);
+        System.out.println(fl);
+        System.out.println(db);
+        System.out.println(bl);
+    }
+}

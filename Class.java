7411fefb5abacefa4e34fe0e8e12774aec46014e
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-01 11:12:46 +0530

    method overloading in java

--- Class.java
+++ Class.java
@@ -1,8 +1,14 @@
 class Calculator {
     public int add(int a, int b) {
-        int res = a + b;
-        // System.out.println("Added: " + res);
-        return res;
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public double add(double a, int b) {
+        return a + b;
     }
 }
 
@@ -10,8 +16,9 @@ public class Class {
     public static void main(String[] args) {
         int a = 5;
         int b = 9;
+        int c = 9;
         Calculator calc = new Calculator();
-        int res = calc.add(a, b);
+        int res = calc.add(a, b, c);
         System.out.println("Result: " + res);
     }
 }

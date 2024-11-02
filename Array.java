71277f413eed207aa2aacdbd889ccf421dfc13bd
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-02 12:05:18 +0530

    2D-3D arrays and advanced for loop in java

--- Array.java
+++ Array.java
@@ -1,13 +1,25 @@
 public class Array {
     public static void main(String[] args) {
-        int arr[] = new int[5];
-        arr[0] = 5;
-        arr[1] = 6;
-        arr[2] = 1;
-        arr[3] = 9;
-        arr[4] = 0;
-        for(int i=0; i<5; ++i){
-            System.out.print(i + " ");
+        int arr[][] = new int[3][4];
+        // arr[0] = 5;
+        // arr[1] = 6;
+        // arr[2] = 1;
+        // arr[3] = 9;
+        // arr[4] = 0;
+        // for(int i=0; i<5; ++i){
+        //     System.out.print(i + " ");
+        // }
+        for(int i=0; i<3; ++i){
+            for(int j = 0; j<4; ++j){
+                arr[i][j] = (int)(Math.random()*10);
+            }
+        }
+
+        for(int m[]: arr){
+            for(int n: m){
+                System.out.print(n + " ");
+            }
+            System.out.println();
         }
     }
 }

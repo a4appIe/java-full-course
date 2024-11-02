a6fb57f65183f4d3a5045cce5753181cd6fbb6fd
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-02 18:21:45 +0530

    static methods in java

--- Static.java
+++ Static.java
@@ -1,16 +1,22 @@
 class Game{
     static int score = 0;
+    String player;
     void increaseScore(){
         score++;
+    }
+    static void show(Game game){
+        System.out.println("Player " + game.player + " has " + score + " score.");
     }
 }
 
 public class Static {
     public static void main(String[] args) {
         Game game = new Game();
+        game.player = "Nitin";
         game.increaseScore();
         System.out.println(Game.score);
         game.increaseScore();
         System.out.println(Game.score);
+        Game.show(game);
     }
 }

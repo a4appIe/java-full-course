2461eba2b4ee4c57b064e9dec2db3cab24d207e5
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-02 11:59:11 +0530

    array in java

---
+++ Array.java
@@ -0,0 +1,13 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        arr[0] = 5;
+        arr[1] = 6;
+        arr[2] = 1;
+        arr[3] = 9;
+        arr[4] = 0;
+        for(int i=0; i<5; ++i){
+            System.out.print(i + " ");
+        }
+    }
+}

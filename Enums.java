f3bd9fc518f6628746a0d625c4def2a0b4d1576f
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-05 22:48:54 +0530

    if-else and switch with enums

--- Enums.java
+++ Enums.java
@@ -1,12 +1,34 @@
-enum Status{
-    Running, Failed, Pending, Success; 
+enum Status {
+    Running, Failed, Pending, Success; // objects of Status enum/class // by default are static;;;
 }
 
 public class Enums {
     public static void main(String[] args) {
-        Status status[] = Status.values();
-        for(Status s: status){
-            System.out.println(s);
+        Status status = Status.Running;
+        // for(Status s: status){
+        // System.out.println(s);
+        // }
+        switch (status) {
+            case Running:
+                System.out.println("running..");
+            case Failed:
+                System.out.println("failed..");
+            case Pending:
+                System.out.println("pending..");
+            case Success:
+                System.out.println("success..");
+        }
+
+        if (status == Status.Running) {
+            System.out.println("running..");
+
+        } else if (status == Status.Pending) {
+            System.out.println("pending..");
+
+        } else if (status == Status.Failed) {
+            System.out.println("failed..");
+        } else {
+            System.out.println("success..");
         }
     }
 }

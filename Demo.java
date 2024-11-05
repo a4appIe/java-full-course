37a60e7b952451159e48c5c936f23ab812bd6a52
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-05 13:22:29 +0530

    wrapper class '--Integer' --boxing and --unboxing in java

---
+++ Demo.java
@@ -0,0 +1,13 @@
+public class Demo {
+    public static void main(String[] args) {
+        int num = 5;
+        // Integer num2 = new Integer(num); ------- boxing
+        Integer num2 = num; // ------------- autoboxing
+        // int num3 = num2.intValue();  // -------- unboxing
+        int num3 = num2;  // -------- autounboxing
+
+        
+
+
+    }
+}

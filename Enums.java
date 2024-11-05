c45f12c905758d83615294d4db0523287af15b17
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-05 23:49:15 +0530

    enums as classes

--- Enums.java
+++ Enums.java
@@ -1,41 +1,21 @@
-enum Status {
-    Running, Failed, Pending, Success; // objects of Status enum/class // by default are static;;;
+enum Laptop {
+    Mac(1000), XPS(800), Lenove(1500); // objects of Status enum/class // by default are static;;;
+
+    private int price;
+
+    private Laptop(int price) {
+        this.price = price;
+    }
+    public int getPrice() {
+        return price;
+    }
 }
 
 public class Enums {
     public static void main(String[] args) {
-        Status status = Status.Running;
-        // for(Status s: status){
-        // System.out.println(s);
-        // }
-        switch (status) {
-            case Running:
-                System.out.println("running..");
-                break;
-            case Failed:
-                System.out.println("failed..");
-                break;
-            case Pending:
-                System.out.println("pending..");
-                break;
-            case Success:
-                System.out.println("success..");
-                break;
-            default:
-                System.out.println("default..");
-                break;
-        }
-
-        if (status == Status.Running) {
-            System.out.println("running..");
-
-        } else if (status == Status.Pending) {
-            System.out.println("pending..");
-
-        } else if (status == Status.Failed) {
-            System.out.println("failed..");
-        } else {
-            System.out.println("success..");
+        Laptop lap[] = Laptop.values();
+        for(Laptop x: lap){
+            System.out.println(x + ": $"+x.getPrice());
         }
     }
 }

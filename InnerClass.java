8d327be7390e8a870a6fc76a72c37ad6bc103444
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-05 15:39:51 +0530

    inner class in java

--- InnerClass.java
+++ InnerClass.java
@@ -16,7 +16,9 @@ class Config {
 public class InnerClass {
     public static void main(String[] args) {
         Config config = new Config();
+        config.print();
         // Config.Hello hello =  config.new Hello();
         Config.Hello hi = new Config.Hello();
+        hi.print();
     }
 }

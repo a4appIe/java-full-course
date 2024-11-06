5b719e32552fb53091ee3ef3f0c837c4a88b69b2
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-06 23:22:03 +0530

    inputs in java

---
+++ Inputs.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+
+public class Inputs {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Enter a num >>  ");
+        int num = System.in.read();
+        System.out.println(num - 48);
+    }
+}

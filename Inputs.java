164b86d6a02012e58061bc298aef65268315fa5e
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-07 00:02:12 +0530

    try with resources -- autoClosables in java

--- Inputs.java
+++ Inputs.java
@@ -1,14 +1,15 @@
 // import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 // import java.io.InputStreamReader;
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) throws IOException {
-        System.out.print("Enter your name >>  ");
-        Scanner scanner = new Scanner(System.in);
-        String name = scanner.nextLine();
-        System.out.println(name);
-        scanner.close();
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            int num = Integer.parseInt(br.readLine());
+            System.out.println(num);
+        }
     }
 }

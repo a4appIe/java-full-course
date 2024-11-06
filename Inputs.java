66f47c00d31e8c86f93475806415f1bcc14c2001
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-06 23:25:06 +0530

    bufferedReader in inputStreamReader in java

--- Inputs.java
+++ Inputs.java
@@ -1,9 +1,13 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Inputs {
     public static void main(String[] args) throws IOException {
         System.out.println("Enter a num >>  ");
-        int num = System.in.read();
-        System.out.println(num - 48);
+        InputStreamReader in = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(in);
+        String name = br.readLine();
+        System.out.println(name);
     }
 }

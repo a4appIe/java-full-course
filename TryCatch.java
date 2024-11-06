5d848a88fb8264f5b640ccba07fedf600dbc49d5
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-06 22:50:03 +0530

    custom exception in java

--- TryCatch.java
+++ TryCatch.java
@@ -1,10 +1,16 @@
+class CustomException extends Exception{
+    public CustomException(String str){
+        super(str);
+    }
+}
+
 public class TryCatch {
     public static void main(String[] args) {
         int a = 3;
         try {
             int j = a / 20;
             if (j == 0) {
-                throw new ArithmeticException("Sorry");
+                throw new CustomException("Sorry");
             }
         } catch (ArithmeticException e) {
             System.out.println(e);

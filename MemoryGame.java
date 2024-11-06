d08e4cdf337cc234d5555910e4d6780042889ff3
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-06 22:05:47 +0530

    memory game completed

--- MemoryGame.java
+++ MemoryGame.java
@@ -18,7 +18,7 @@ class Game {
 
     // GENERATE RANDOM CHARACTER FUNCTION
     private char randChar() {
-        return (char) +('0' + (int) (Math.random() * 10));
+        return (char)('0' + (int) (Math.random() * 10));
     }
 
     // GETTING THE INPUT FUNCTION
@@ -54,7 +54,7 @@ class Game {
         this.delay();
         System.out.print(".");
         this.delay();
-        System.out.println(".");
+        System.out.print(".");
         this.delay();
         System.out.println(".");
         this.delay();
@@ -66,11 +66,10 @@ class Game {
             System.out.println("keep practicing | keep playing 😊");
             this.newLine();
             System.out.println("bye👋");
-        } else if (this.score >= 5 && this.score < 10) {
+        } else if (this.score < 10) {
             this.newLine();
             System.out.println(
                     this.name.toUpperCase() + ", your memory power is average!" + " | your score is: " + this.score);
-
             this.newLine();
             System.out.println("keep practicing | keep playing 😊");
             this.newLine();
@@ -89,8 +88,8 @@ class Game {
         this.newLine();
     }
 
-    // INITIALIZING GAME FUNCTION
-    private void initializeGame() {
+    // EASY GAME FUNCTION
+    private void easy() {
         while (flag) {
             // APPEND THE CHAR TO COMPUTER VARIABLE
             computer.append(this.randChar());
@@ -126,6 +125,104 @@ class Game {
                 this.score++;
                 System.out.println("keep going");
                 this.delay();
+                Game.clear();
+                this.delay();
+                flag = true;
+            } else {
+                flag = false;
+                this.gameEnd();
+            }
+        }
+    }
+
+    private void medium() {
+        int size = 1;
+        final int maxSize = 100;
+        while (flag) {
+            // APPEND THE CHAR TO COMPUTER VARIABLE
+            for (int i = 0; i < size; i++) {
+                computer.append(this.randChar());
+            }
+            size = Math.min(size + 1, maxSize);
+            this.delay();
+
+            // // PRINT THE DISCLAIMER
+            System.out.println("Note: Type without spaces!          Score: " + this.score);
+            this.newLine();
+
+            // // PRINT THE LETTER ONE BY ONE
+            System.out.print("try this:- [ ");
+            for (int i = 0; i < computer.length(); ++i) {
+                this.delay();
+                System.out.print(computer.charAt(i) + " ");
+                this.delay();
+            }
+            System.out.print("]");
+            this.delay();
+
+            // // CLEAR THE CONSOLE
+            Game.clear();
+
+            // // KEEP DISCLAIMER AND SCORE ON CONSOLE
+            System.out.println("Note: Type without spaces!          Score: " + this.score);
+            this.newLine();
+
+            // // GET INPUT FROM USER
+            String input = this.getInput();
+            Game.clear();
+
+            // // CHECK IF MATCHES WITH COMPUTER
+            if (computer.toString().equals(input)) {
+                this.score++;
+                computer.setLength(0);
+                this.delay();
+                this.delay();
+                Game.clear();
+                this.delay();
+                flag = true;
+            } else {
+                flag = false;
+                this.gameEnd();
+            }
+        }
+    }
+
+    private void hard() {
+        int size = 1;
+        final int maxSize = 100;
+        while (flag) {
+            // APPEND THE CHAR TO COMPUTER VARIABLE
+            for (int i = 0; i < size; i++) {
+                computer.append(this.randChar());
+            }
+            size = Math.min(size + 1, maxSize);
+            this.delay();
+
+            // // PRINT THE LETTER ONE BY ONE
+            for (int i = 0; i < computer.length(); ++i) {
+                System.out.print(">> ");
+                this.delay();
+                System.out.print(computer.charAt(i) + " ");
+                this.delay();
+                Game.clear();
+            }
+            this.delay();
+
+            // // CLEAR THE CONSOLE
+            Game.clear();
+
+            // // KEEP DISCLAIMER AND SCORE ON CONSOLE
+            System.out.println("Note: Type without spaces!          Score: " + this.score);
+            this.newLine();
+
+            // // GET INPUT FROM USER
+            String input = this.getInput();
+            Game.clear();
+
+            // // CHECK IF MATCHES WITH COMPUTER
+            if (computer.toString().equals(input)) {
+                this.score++;
+                computer.setLength(0);
                 this.delay();
                 Game.clear();
                 this.delay();
@@ -138,16 +235,13 @@ class Game {
     }
 
     // START FUNCTION
-    public void start() {
-        this.newLine();
-        System.out.println("game started |  all the best  -->>  " + this.name.toLowerCase());
-        this.delay();
-        this.newLine();
-        System.out.println("checking your memory power");
-        this.delay();
-        this.delay();
-        Game.clear();
-        this.initializeGame();
+    public void start(int input) {
+        switch (input) {
+            case 1 -> this.easy();
+            case 2 -> this.medium();
+            case 3 -> this.hard();
+            default -> System.out.println("Please make a valid choice next time");
+        }
     }
 }
 
@@ -160,10 +254,19 @@ public class MemoryGame {
         return scanner.nextLine();
     }
 
+    public static int input(Scanner scanner) {
+        System.out.print("Set difficulty 💀 (1: easy, 2: medium, 3: hard) >>   ");
+        int difficulty = scanner.nextInt();
+        scanner.nextLine();  // Consume the leftover newline character
+        return difficulty;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String name = getName(scanner);
+        int input = input(scanner);
         Game game = new Game(name, scanner);
-        game.start();
+        game.start(input);
+        scanner.close();
     }
 }

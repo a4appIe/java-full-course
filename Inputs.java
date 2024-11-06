b3e328f729cbb40a035892c97f7350a340dfdad3
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-06 23:28:37 +0530

    scanner class in java

--- Inputs.java
+++ Inputs.java
@@ -1,13 +1,14 @@
-import java.io.BufferedReader;
+// import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+// import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Inputs {
     public static void main(String[] args) throws IOException {
-        System.out.println("Enter a num >>  ");
-        InputStreamReader in = new InputStreamReader(System.in);
-        BufferedReader br = new BufferedReader(in);
-        String name = br.readLine();
+        System.out.print("Enter your name >>  ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
         System.out.println(name);
+        scanner.close();
     }
 }

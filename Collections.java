f012d2e957d94ac7ec6131a756882e16547ad3e4
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-07 14:23:19 +0530

    HashSet and TreeSet in java

--- Collections.java
+++ Collections.java
@@ -1,13 +1,19 @@
-import java.util.List;
-import java.util.ArrayList;
+// import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+// import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Collections {
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<Integer>();
+        Set<Integer> nums = new HashSet<Integer>();
+        // Set<Integer> nums = new TreeSet<Integer>();
         nums.add(5);
+        nums.add(8);
+        nums.add(6);
         nums.add(6);
         for (Object n : nums) {
-            System.out.println((Integer) n);
+            System.out.println(n);
         }
     }
 }

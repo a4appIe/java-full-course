cdad5892c06240364b989f22d5b57eaa76885d14
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-07 12:21:41 +0530

    runnable and lambda expression

--- Threads.java
+++ Threads.java
@@ -1,14 +1,14 @@
-class A implements Runnable{
-    public void run(){
-        for(int i=0; i<100; i++){
+class A implements Runnable {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
             System.out.println("hi");
         }
     }
 }
 
-class B implements Runnable{
-    public void run(){
-        for(int i=0; i<100; i++){
+class B implements Runnable {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
             System.out.println("hello");
         }
     }
@@ -16,8 +16,16 @@ class B implements Runnable{
 
 public class Threads {
     public static void main(String[] args) {
-        Runnable a = new A();
-        Runnable b = new B();
+        Runnable a = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("hi");
+            }
+        };
+        Runnable b = () -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println("hello");
+            }
+        };
         Thread c = new Thread(a);
         Thread d = new Thread(b);
         c.start();

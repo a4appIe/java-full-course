1ab82cb8b0287356cb9b8e00984fe0ef01dd036f
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-07 23:30:54 +0530

    comparable interface in java

--- Collectionss.java
+++ Collectionss.java
@@ -3,27 +3,42 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.Collections;
 
+class Stu implements Comparable<Stu> {
+    int age;
+    String name;
+
+    public Stu(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String toString() {
+        return "Student : [ " + this.name + ", Age : " + this.age + " ]";
+    }
+
+    public int compareTo(Stu x) {
+        return this.age > x.age ? 1 : -1;
+    }
+
+}
+
 public class Collectionss {
     public static void main(String[] args) {
-        Comparator<Integer> com = new Comparator<Integer>() {
-            public int compare(Integer i, Integer j) {
-
-                if (i % 10 > j % 10)
-                    return 1;
-                else
-                    return -1;
-            }
-        };
-
-        List<Integer> list = new ArrayList<>();
-        list.add(45);
-        list.add(62);
-        list.add(17);
-        list.add(90);
-        list.add(34);
-
-        Collections.sort(list, com);
-        System.out.println(list);
+        // Comparator<Integer> com = (i, j) -> i%10 > j%10 ? 1 : -1;
+        List<Stu> list = new ArrayList<>();
+        list.add(new Stu(23, "Nitin"));
+        list.add(new Stu(20, "Aryan"));
+        list.add(new Stu(22, "Vijay"));
+        list.add(new Stu(27, "Mahi"));
+        for (Stu s : list) {
+            System.out.println(s);
+        }
+        System.out.println();
+        Collections.sort(list);
+        System.out.println();
+        for (Stu s : list) {
+            System.out.println(s);
+        }
 
     }
 }
